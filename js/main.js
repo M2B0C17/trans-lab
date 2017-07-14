d260fe38f12096a68492eb49759007c11432e7dc
macarena baltra <macarena.bc2011@gmail.com>
2017-07-14 13:57:37 -0400

    Validaciones - Intentos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+	// Validación HTML 1 - No funciona la redirección con href
+	$("#botonIniciarSesion").click(function() {
+		var correo = $("#icon_prefix").val();
+		var contra = $("#lock").val(); // variable donde conectadmos con la funcion en el html
+
+		localStorage.correo = correo; // guardando datos
+		localStorage.cont = contra; // guardando datos
+
+		var validacionCorreo = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		var validacionContra = /^([0-9])*$/;
+
+		if (validacionCorreo.test(correo) && validacionContra.test(contra) && contra.lenght <= 8) { // condiciones a cumplir
+			window.location.href = 'index-2.html'; // redireccionar pagina
+		}else{
+			alert("Debes escribir en los campos obligatorios"); // mensaje de error
+		}
+	});
+
+
 	// Iniciando select
     $('select').material_select();
-    
 
 });
